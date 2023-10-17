560f39fb00d2862891b432294f99d450e0abc9e3
Taha-AhmedK <tahaahmedkhn@gmail.com>
2023-10-17 11:08:18 +0500

    Part 8_6

---
+++ part08-Part08_06.Nicknames/src/main/java/Nicknames.java
@@ -0,0 +1,17 @@
+
+import java.util.HashMap;
+
+public class Nicknames {
+
+    public static void main(String[] args) {
+        // Do the operations required here!
+        
+        HashMap<String , String> map = new HashMap<>();
+        map.put("matthew", "matt");
+        map.put("arthur","artie");
+        map.put("michael", "mix");
+        
+        System.out.println(map.get("matthre"));
+    }
+
+}
